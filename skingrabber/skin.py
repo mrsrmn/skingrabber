225aad85f88382787dd28ab997acc6db7034095f
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-15 16:53:16 +0300

    Fixed some bugs

--- skingrabber/skin.py
+++ skingrabber/skin.py
@@ -8,39 +8,36 @@ class skingrabber:
     def __init__(self):
         self.url = "https://api.mojang.com/users/profiles/minecraft/"
 
-    def get_uuid(self, user: str):
-
+    def get_uuid(self, user):
         """user is the nickname of the player in this case"""
-        print("Searching...")
+
         response = requests.get(self.url + user)
         if response.status_code != 200:
-            print("That player doesn't seem to exist")
+            return None
         else:
             responsejson = json.loads(response.content)
 
             uuid = responsejson["id"]
             return uuid
 
-    def get_skin(self, user: str):
+    def get_skin(self, user):
         """The 2D version of the users skin"""
 
-        print("Searching...")
         response = requests.get(self.url + user)
         if response.status_code != 200:
-            print("That player doesn't seem to exist!")
+            return None
         else:
             responsejson = json.loads(response.content)
 
             uuid = responsejson["id"]
             return f"https://crafatar.com/skins/{uuid}"
 
-    def get_skin_rendered(self, user: str):
+    def get_skin_rendered(self, user):
         """The 3D rendered version of the users skin"""
 
-        print("Searching...")
         response = requests.get(self.url + user)
         if response.status_code != 200:
-            print("That player doesn't seem to exist!")
+            return None
         else:
             responsejson = json.loads(response.content)
 
